a8b8699b39d2457bb08ed6205d9dd4ff2c51d3f9
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-03-08 22:27:38 -0600

    Pretrain and train/evaluate

--- teacher_student_train.py
+++ teacher_student_train.py
@@ -26,7 +26,7 @@ def teacher_student_train(seed, batch_size=8, epoch=1, dir_base="/home/zmh001/r-
     IMG_SIZE = 384
     # IMG_SIZE = 600
     BATCH_SIZE = batch_size
-    LR = 1e-7 #8e-5  # 1e-4 was for efficient #1e-06 #2e-6 1e-6 for transformer 1e-4 for efficientnet
+    LR = 8e-5  # 1e-4 was for efficient #1e-06 #2e-6 1e-6 for transformer 1e-4 for efficientnet
     GAMMA = 0.7
     N_EPOCHS = epoch  # 8
     N_CLASS = n_classes
