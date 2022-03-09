f7297c21c6405cbe522706f3f77ebc1a60a32fd5
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-03-09 00:20:58 -0600

    Use bioclincial bert

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+import os
 
 from teacher_student_train import teacher_student_train
 from train_vision_model import train_vision_model
@@ -19,9 +21,20 @@ if __name__ == '__main__':
         directory_base = "/UserData/"
 
     seeds = [117, 295, 714, 892, 1023]
+    accuracy_list = []
 
     for seed in seeds:
         teacher_student_train(seed=seed, batch_size=3, epoch=10, dir_base=directory_base, n_classes=5)
         acc, matrix = train_vision_model(seed=seed, batch_size=3, epoch=10, dir_base=directory_base, n_classes=5)
+
+        accuracy_list.append(acc)
+        df = pd.DataFrame(matrix)
+
+        ## save to xlsx file
+        filepath = os.path.join(directory_base, '/UserData/Zach_Analysis/result_logs_teach_student/test/confusion_matrix_seed' + str(seed) + '.xlsx')
+
+        df.to_excel(filepath, index=False)
+
+        print(accuracy_list)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
